33f9baf190a4e9d317276dec5f0eb52baf35eec3
Wu Jane <jane72418@gmail.com>
2022-10-31 17:11:06 +0800

    Update 2022-10-31T09:10:49.412Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,87 +1,83 @@
+"use strict";
+
 //navbar
-window.addEventListener("scroll", (e) => {
-  const navbar = document.querySelector("nav");
-  const windowScrollY = window.scrollY;
+window.addEventListener("scroll", function (e) {
+  var navbar = document.querySelector("nav");
+  var windowScrollY = window.scrollY;
+
   if (windowScrollY > 0) {
     navbar.classList.add("bg-blur-dark");
   } else if (windowScrollY <= 0) {
     navbar.classList.remove("bg-blur-dark");
   }
-});
+}); //swiper
 
-//swiper
 var swiper = new Swiper(".game-swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    prevEl: ".swiper-button-prev"
   },
   autoplay: {
-    delay: 3000,
+    delay: 3000
   },
-
   breakpoints: {
     // when window width is >= 320px
     280: {
-      slidesPerView: 1,
+      slidesPerView: 1
     },
-
     768: {
-      slidesPerView: 2,
+      slidesPerView: 2
     },
     992: {
-      slidesPerView: 4,
-    },
+      slidesPerView: 4
+    }
   },
-  loop: true,
+  loop: true
 });
-
 var swiper = new Swiper(".comment-swiper", {
   pagination: {
     el: ".swiper-pagination",
     type: "bullets",
-    clickable: true,
+    clickable: true
   },
   autoplay: {
-    delay: 3000,
+    delay: 3000
   },
   breakpoints: {
     // when window width is >= 320px
     280: {
-      slidesPerView: 1,
+      slidesPerView: 1
     },
     768: {
-      slidesPerView: 2,
+      slidesPerView: 2
     },
     992: {
-      slidesPerView: 3,
-    },
-  },
-  loop: true,
-});
-
-//countup
-function countNum(endNum, target) {
-  $({ countNum: 0 }).animate(
-    {
-      countNum: endNum,
-    },
-    {
-      duration: 5000,
-      easing: "linear",
-      step: function () {
-        target.text(Math.floor(this.countNum));
-      },
-      complete: function () {
-        target.text(this.countNum);
-      },
+      slidesPerView: 3
     }
-  );
-}
+  },
+  loop: true
+}); //countup
 
-// if ($(window.scrollY) > $(".comment".offsetTop)) {
-const comment = document.querySelector(".comment");
+function countNum(endNum, target) {
+  $({
+    countNum: 0
+  }).animate({
+    countNum: endNum
+  }, {
+    duration: 5000,
+    easing: "linear",
+    step: function step() {
+      target.text(Math.floor(this.countNum));
+    },
+    complete: function complete() {
+      target.text(this.countNum);
+    }
+  });
+} // if ($(window.scrollY) > $(".comment".offsetTop)) {
 
-window.addEventListener("scroll", (e) => {
+
+var comment = document.querySelector(".comment");
+window.addEventListener("scroll", function (e) {
   if (window.scrollY <= comment.offsetTop - 1300) {
     // setTimeout(countNum(8, $("#game-num")), 5000);
     console.log("hui");
@@ -94,33 +90,18 @@ window.addEventListener("scroll", (e) => {
   } else {
     return;
   }
-});
+}); //calendar
 
-//calendar
 $(document).ready(function () {
   $("#datepicker").datepicker();
-  $("#datepicker").datepicker("option", "monthNames", [
-    "一月",
-    "二月",
-    "三月",
-    "四月",
-    "五月",
-    "六月",
-    "七月",
-    "八月",
-    "九月",
-    "十月",
-    "十一月",
-    "十二月",
-  ]);
+  $("#datepicker").datepicker("option", "monthNames", ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"]);
   $("#datepicker").datepicker("option", "yearRange", "2022:2023");
-});
+}); //parallax
 
-//parallax
-window.addEventListener("scroll", (e) => {
+window.addEventListener("scroll", function (e) {
   $("#game-banner").css("opacity", 1 - window.scrollY / 700);
-  $("#game-banner").css("top", window.scrollY + "px");
-  // if ($("#game-banner").css("opacity") === 0) {
+  $("#game-banner").css("top", window.scrollY + "px"); // if ($("#game-banner").css("opacity") === 0) {
   //   $("#game-banner").toggle("display", "none");
   // }
 });
+//# sourceMappingURL=all.js.map
